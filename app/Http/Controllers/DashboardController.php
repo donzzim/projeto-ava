15dae7c8b4f7c736b6c624154f330b8274e57010
Lucas César <lucascesarmengo@gmail.com>
2025-12-16 14:10:27 -0300

    email middleware

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct(){
+        // $this->middleware('auth')->except(['index', 'produtos', 'categorias']);
+        // $this->middleware('auth')->only(['index', 'produtos', 'categorias']);
+        // Colocar auth antes de acessar certas paginas
+    }
+
     public function index(){
         return view('admin.dashboard');
     }
---
+++ app/Http/Middleware/CheckEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckEmail
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Verificar se o email = gmail
+        if(!auth()->check()){
+            return redirect(route('login'));
+        }
+
+        $email = auth()->user()->email;
+        $data = explode('@', $email);
+        $servidorEmail = $data[1];
+
+        if($servidorEmail != 'gmail.com'){
+            return redirect(route('login'));
+        }
+
+        return $next($request);
+    }
+}
